b01d71c0ae3f3095ce5eb4655975477b2f5b3e06
Luis Ruiz Mediavilla <lruiz@werfen.com>
2025-09-03 14:49:07 +0200

    added sources updated to conan2

---
+++ conan_export.bat
@@ -0,0 +1,2 @@
+conan export-pkg . --name=loki --version=1.1.0 --profile:build=.conanprofile -s build_type=Debug
+conan export-pkg . --name=loki --version=1.1.0 --profile:build=.conanprofile -s build_type=Release
--- conanfile.py
+++ conanfile.py
@@ -1,34 +1,49 @@
 from conan import ConanFile
 from conan.errors import ConanInvalidConfiguration
-from conan.tools.files import collect_libs
-from conan.tools.microsoft import MSBuild
-
+from conan.tools.files import copy, collect_libs
+from conan.tools.microsoft import MSBuild, MSBuildToolchain, MSBuildDeps
+import os
 
 class LokiConan(ConanFile):
-	name = "Loki"
-	description = "C++ library of designs, containing flexible implementations of common design patterns and idioms"
-	author = "CSW <csw@werfen.com>"
-	topics = ("conan", "loki")
-	license = "MIT"
-	package_type = "library"
-	settings = "os", "compiler", "build_type", "arch"
+    name = "loki"
+    description = "C++ library of designs, containing flexible implementations of common design patterns and idioms"
+    author = "CSW <csw@werfen.com>"
+    topics = ("conan", "loki")
+    license = "MIT"
+    settings = "os", "compiler", "build_type", "arch"
 
-	def build(self):
-		msbuild = MSBuild(self)
-		msbuild.build_type = self.settings.build_type
-		arch = self.settings.get_safe('arch')
-		if arch == 'x86':
-			msbuild.platform = 'Win32'
-		else:
-			raise ConanInvalidConfiguration(f"Loki does not support '{arch}' architecture")
-		msbuild.build(sln='Loki.sln', targets=['Library'])
+    exports_sources = (
+        "src/*",
+        "include/*",
+        "test/*",
+        "Loki.sln"
+    )
 
-	def package(self):
-		binaries_folder = f"lib/{self.settings.build_type}"
-		
-		self.copy("*.h", dst="include/loki", src="include/loki")
-		self.copy("*.lib", dst="lib", src=binaries_folder)
-		self.copy("*.pdb", dst="lib", src=binaries_folder)
+    def build(self):
+        # Genera la toolchain (defines, runtime, etc.)
+        msbuild_tc = MSBuildToolchain(self)
+        msbuild_tc.generate()
 
-	def package_info(self):
-		self.cpp_info.libs = collect_libs(self)
+        # Configuración de MSBuild
+        msbuild = MSBuild(self)
+
+        arch = str(self.settings.arch)
+        if arch == "x86":
+            msbuild.platform = "Win32"
+        else:
+            raise ConanInvalidConfiguration(f"Loki does not support '{arch}' architecture")
+
+        msbuild.build(
+            sln="Loki.sln",
+            targets=["Library"]
+        )
+
+    def package(self):
+        binaries_folder = os.path.join(self.source_folder, "lib", str(self.settings.build_type)) 
+        
+        copy(self, "*.h", dst=os.path.join(self.package_folder, "include", "loki"), src=os.path.join(self.source_folder, "include", "loki"))
+        copy(self, "*.lib", dst=os.path.join(self.package_folder, "lib"), src=binaries_folder)
+        copy(self, "*.pdb", dst=os.path.join(self.package_folder, "lib"), src=binaries_folder)
+
+    def package_info(self):
+        self.cpp_info.libs = collect_libs(self)
